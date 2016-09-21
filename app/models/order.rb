eb94a0d4041512996197f8df24e64caee95055ff
pudge <haider@isometrylabs.com>
2016-09-21 12:37:57 +0500

    more formatting

--- app/models/order.rb
+++ app/models/order.rb
@@ -14,28 +14,28 @@ class Order
 
 
   def get_price
-  	self.order_items.sum(&:price)
+    self.order_items.sum(&:price)
 	end 
 
 	def add_item(item_id, flavor)
-		@new_item= MenuItem.find(item_id)
-		flavor= @new_item.flavors.sample if flavor == "random"
+	  @new_item= MenuItem.find(item_id)
+	  flavor= @new_item.flavors.sample if flavor == "random"
     @new_order_item = self.order_items.new price: @new_item.price, flavor: flavor
     @new_order_item.menu_item = @new_item
     @new_order_item.save!
   end
 
   def remove_item(item_id)
-  	OrderItem.find(item_id).delete
+    OrderItem.find(item_id).delete
     self.delete! if self.order_items.count == 0
   end
 
   def self.reorder(order,user)
-		@new_order = user.orders.create is_placed?:true
+	  @new_order = user.orders.create is_placed?:true
     order.order_items.each do |item|
-    new_item = item.clone
-    new_item.order = @new_order
-    new_item.save!
+      new_item = item.clone
+      new_item.order = @new_order
+      new_item.save!
     end
   end
 
